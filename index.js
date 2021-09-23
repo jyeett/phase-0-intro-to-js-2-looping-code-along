85633edbe9da7cece6398c57773cfaba6fefc490
Jordan Yee <jordanyee799@gmail.com>
2021-09-23 14:26:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let nameCards = [];
+    for (let i = 0; i < names.length; i++) {
+        nameCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return nameCards;
+}
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--;
+    }
+}
